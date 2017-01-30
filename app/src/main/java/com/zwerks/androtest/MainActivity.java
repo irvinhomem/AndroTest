36fa516ad4686106a311a47da2b26f03fa22e900
Irvin Homem <irvinhomem@gmail.com>
2017-01-30 19:36:38 +0100

    Added new Activity - DisplayMessageActivity -  and metadata around it

--- app/src/main/java/com/zwerks/androtest/MainActivity.java
+++ app/src/main/java/com/zwerks/androtest/MainActivity.java
@@ -9,7 +9,12 @@ import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import android.content.Intent;
+import android.widget.EditText;
+
+
 public class MainActivity extends AppCompatActivity {
+    public final static String EXTRA_MESSAGE = "com.zwerks.androtest.MESSAGE";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -54,6 +59,10 @@ public class MainActivity extends AppCompatActivity {
     /** Called when the user clicks the "Send" button */
     public void sendMessage(View view){
         //Do something in response to button click
-
+        Intent intent = new Intent(this, DisplayMessageActivity.class);
+        EditText editText = (EditText) findViewById(R.id.edit_message);
+        String message = editText.getText().toString();
+        intent.putExtra(EXTRA_MESSAGE, message);
+        startActivity(intent);
     }
 }
