f1339b67d93bbdf89fd43cdf0c14b3d0434f9897
Irvin Homem <irvinhomem@gmail.com>
2017-01-31 20:24:47 +0100

    Playing with the Appcompat ActionBar/Toolbar. Adding drawable resources and Button actions to Menu and Overflow Menu. Adding string resources.

--- app/src/main/java/com/zwerks/androtest/MainActivity.java
+++ app/src/main/java/com/zwerks/androtest/MainActivity.java
@@ -20,14 +20,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+        // Set the "activity_main.xml" layout as the current View
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+
+        //Find the view (item) with the id "toolbar"
+        Toolbar toolbar = (Toolbar) findViewById(R.id.my_toolbar);
+        // Set a "toolbar" as the ActionBar (from the Android Dev Docs)
         setSupportActionBar(toolbar);
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                // Snackbar is the small horizonal bar that pops-in at the bottom (like a toast but from the bottom)
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
             }
@@ -41,6 +46,7 @@ public class MainActivity extends AppCompatActivity {
         return true;
     }
 
+    /*
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         // Handle action bar item clicks here. The action bar will
@@ -55,6 +61,39 @@ public class MainActivity extends AppCompatActivity {
 
         return super.onOptionsItemSelected(item);
     }
+    */
+
+    /**/
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+
+        /** You can choose to use either an "if...else" or a "switch ...case" */
+        switch (item.getItemId()) {
+            case R.id.action_settings:
+                // User chose the "Settings" item, show the app settings UI...
+                return true;
+
+            case R.id.action_favorite:
+                // User chose the "Favorite" action, mark the current item
+                // as a favorite...
+                return true;
+
+            case R.id.action_about:
+                //User chose the "About" action ... so show the "About" activity or pop up of some sort
+                return true;
+
+            default:
+                // If we got here, the user's action was not recognized.
+                // Invoke the superclass to handle it.
+                return super.onOptionsItemSelected(item);
+
+        }
+    }
+
+    /* */
 
     /** Called when the user clicks the "Send" button */
     public void sendMessage(View view){
